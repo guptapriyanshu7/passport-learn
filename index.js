d1e9bab9358e80db849bf9740943c8aca9f9a55c
Priyanshu <guptapriyanshu71@gmail.com>
2020-09-21 16:25:20 +0530

    change

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import "dotenv/config.js";
 import express from "express";
-import logger from "morgan";
 import bodyParser from "body-parser";
 import config from "./config.js";
 import routes from "./routes/index.js";
@@ -8,6 +7,7 @@ import mongoose from "mongoose";
 import passport from "passport";
 import User from "./models/users.js";
 import session from "express-session";
+// import logger from "morgan";
 
 const app = express();
 
