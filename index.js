fd803a8b30172c4bbf6260cd6cbc0b050d8e4e62
Priyanshu <guptapriyanshu71@gmail.com>
2020-09-21 15:32:54 +0530

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import "dotenv/config.js";
 import express from "express";
-import logger from "morgan";
+// import logger from "morgan";
 import bodyParser from "body-parser";
 import config from "./config.js";
 import routes from "./routes/index.js";
@@ -51,9 +51,9 @@ mongoose
 
 app.use("/api/v1", routes(router));
 
-if (environment !== "production") {
-  app.use(logger("dev"));
-}
+// if (environment !== "production") {
+//   app.use(logger("dev"));
+// }
 
 app.use("/api/v1", (req, res, next) => {
   res.send("Hello");
